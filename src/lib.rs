c27a2fa4f396dbf4d55c4164ce647bb2b892796a
Vasiliy Kovalev <29655572+vasiliykovalev@users.noreply.github.com>
2020-12-18 22:24:33 +0300

    Add first implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,57 @@
 use std::sync::{Arc, Condvar, Mutex};
+use std::collections::VecDeque;
 
 pub struct Sender<T> {
     inner: Arc<Inner<T>>
 }
 
-pub struct Receiver<T> {
-    inner: Arc<Inner<T>>
+impl<T> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        Sender {
+            inner: Arc::clone(&self.inner)
+        }
+    }
 }
 
+impl<T> Sender<T> {
+    pub fn send(&mut self, t: T) {
+        let mut queue = self.inner.queue.lock().unwrap();
+        queue.push_back(t);
+        drop(queue);
+        self.inner.available.notify_one();
+    }
+}
+
+pub struct Receiver<T> {
+    inner: Arc<Inner<T>>,
+}
+
+impl<T> Receiver<T> {
+    pub fn recv(&mut self) -> T {
+        let mut queue = self.inner.queue.lock().unwrap();
+        loop {
+            match queue.pop_front() {
+                Some(t) => return t,
+                None => queue = self.inner.available.wait(queue).unwrap()
+            }
+        }
+    }
+
+    pub fn try_recv(&mut self) -> Option<T> {
+        let mut queue = self.inner.queue.lock().unwrap();
+        queue.pop_front()
+    }
+}
 
 struct Inner<T> {
-    queue: Mutex<Vec<T>>,
+    queue: Mutex<VecDeque<T>>,
+    available: Condvar,
 }
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let inner = Inner {
-        queue: Mutex::default()
+        queue: Mutex::default(),
+        available: Condvar::new(),
     };
     let inner = Arc::new(inner);
     (Sender { inner: inner.clone() }, Receiver { inner: inner.clone() })
