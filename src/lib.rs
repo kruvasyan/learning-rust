408677ea824a45b4e438b283b4ff884b89a5aa63
Vasiliy Kovalev <29655572+vasiliykovalev@users.noreply.github.com>
2020-12-18 21:18:37 +0300

    Add channel constructor implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
-pub struct Sender<T> {}
+use std::sync::{Arc, Condvar, Mutex};
 
-pub struct Receiver<T> {}
+pub struct Sender<T> {
+    inner: Arc<Inner<T>>
+}
 
-pub fn channel<T>() -> (Sender<T>, Receiver<T>) {}
+pub struct Receiver<T> {
+    inner: Arc<Inner<T>>
+}
+
+
+struct Inner<T> {
+    queue: Mutex<Vec<T>>,
+}
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let inner = Inner {
+        queue: Mutex::default()
+    };
+    let inner = Arc::new(inner);
+    (Sender { inner: inner.clone() }, Receiver { inner: inner.clone() })
+}
 
