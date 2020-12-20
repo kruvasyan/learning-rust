2e801471d1c84ac426876aabc59484d4e53f938a
Vasiliy Kovalev <29655572+vasiliykovalev@users.noreply.github.com>
2020-12-20 23:27:45 +0300

    Add buffer for Receiver with neat mem::swap optimisation

--- src/lib.rs
+++ src/lib.rs
@@ -40,14 +40,23 @@ impl<T> Sender<T> {
 
 pub struct Receiver<T> {
     shared: Arc<Shared<T>>,
+    buffer: VecDeque<T>,
 }
 
 impl<T> Receiver<T> {
     pub fn recv(&mut self) -> Option<T> {
+        if let Some(t) = self.buffer.pop_front(){
+            return Some(t);
+        }
         let mut inner = self.shared.inner.lock().unwrap();
         loop {
             match inner.queue.pop_front() {
-                Some(t) => return Some(t),
+                Some(t) => {
+                    if !inner.queue.is_empty(){
+                        std::mem::swap(&mut self.buffer, &mut inner.queue)
+                    }
+                    return Some(t)
+                }
                 None if inner.senders == 0 => return None,
                 None => inner = self.shared.available.wait(inner).unwrap()
             }
