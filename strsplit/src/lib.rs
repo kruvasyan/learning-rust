8d1afb214920b87b3996a269be7db3fc850f32d1
Vasiliy Kovalev <vasiliy.kovalev.dev@gmail.com>
2021-01-30 23:27:22 +0300

    Add naive implementation

--- strsplit/src/lib.rs
+++ strsplit/src/lib.rs
@@ -1,10 +1,19 @@
 //!
 #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 
-pub struct StrSplit {}
+pub struct StrSplit {
+    remainder: &str,
+    delimiter: &str,
+
+}
 
 impl StrSplit {
-    pub fn new(haystack: &str, delimiter: &str) -> Self {}
+    pub fn new(haystack: &str, delimiter: &str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter,
+        }
+    }
 }
 
 
@@ -12,18 +21,24 @@ impl Iterator for StrSplit {
     type Item = &str;
 
     fn next(&mut self) -> Option<Self::Item> {
-        unimplemented!()
+        if let Some(next_delim) = self.remainder.find(self.delimiter) {
+            let until_delimiter = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
+        } else if self.remainder.is_empty() {
+            // TODO: bug
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = &[];
+            Some(rest)
+        }
     }
 }
 
 #[test]
 fn it_works() {
     let haystack = "a b c d e";
-    for letter in StrSplit::new(haystack, " ") {
-        // a
-        // b
-        // c
-        // d
-        // e
-    }
+    let letters = StrSplit::new(haystack, " ");
+    assert_eq!(letters, vec!["a", "b", "c", "d", "e"].into_iter())
 }
