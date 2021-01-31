fdb863eae69f993bcd832dcb37fb3d9948508f75
Vasiliy Kovalev <vasiliy.kovalev.dev@gmail.com>
2021-01-31 20:01:19 +0300

    strsplit: refactor impl Iterator

--- strsplit/src/lib.rs
+++ strsplit/src/lib.rs
@@ -21,16 +21,13 @@ impl<'a> Iterator for StrSplit<'a> {
     type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(ref mut remainder) = self.remainder {
-            if let Some(next_delim) = remainder.find(self.delimiter) {
-                let until_delimiter = &remainder[..next_delim];
-                *remainder = &remainder[(next_delim + self.delimiter.len())..];
-                Some(until_delimiter)
-            } else {
-                self.remainder.take()
-            }
+        let remainder = self.remainder.as_mut()?;
+        if let Some(next_delim) = remainder.find(self.delimiter) {
+            let until_delimiter = &remainder[..next_delim];
+            *remainder = &remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
         } else {
-            None
+            self.remainder.take()
         }
     }
 }
